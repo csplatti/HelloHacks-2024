c1ab67fa0c760981a982fc5bf2f0f3bda9aeefce
corbin-fa <csplatti@gmail.com>
2024-10-05 16:05:03 -0700

    OpenAI API Setup

--- Backend/Python/ScoreMe Python Backend/gpt-prompting.py
+++ Backend/Python/ScoreMe Python Backend/gpt-prompting.py
@@ -9,3 +9,17 @@ GPT_API_KEY = os.getenv("GPT_API_KEY")
 print(GPT_API_KEY)
 
 print(sys.executable)
+
+#chatgpt prompting
+from openai import OpenAI
+client = OpenAI(api_key=GPT_API_KEY)
+
+response = client.chat.completions.create(
+    messages=[{
+        "role": "user",
+        "content": "Say this is a test",
+    }],
+    model="gpt-4o-mini",
+).choices[0].message.content
+
+print(response)
